f168d8c155f1bec57152ed8e3d1402dab0f86b28
dan1114ho <dan1114ho@gmail.com>
2018-03-21 16:26:25 -0400

    Finished stripping all php stuff

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,33 +1,11 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 import Vue from 'vue'
 import router from './router'
 
-// window.Vue = require('vue');
-
-
 import App from './components/App'
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 new Vue({
     el: '#app',
     router,
     render: h => h(App)
 })
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-// const app = new Vue({
-//     el: '#app'
-// });
