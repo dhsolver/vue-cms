51c2df6ac88c055fc184808cdaa2edcd533e3fea
dan1114ho <dan1114ho@gmail.com>
2018-08-08 16:14:04 -0400

    Removed reference to unused google api key

--- js/config.js
+++ js/config.js
@@ -9,7 +9,6 @@ let urls = {
 
 let keys = {
     FACEBOOK_APP_ID: process.env.NODE_ENV == 'development' ? process.env.MIX_DEV_FACEBOOK_APP_ID : process.env.MIX_PROD_FACEBOOK_APP_ID,
-    GOOGLE_API_KEY: process.env.NODE_ENV == 'development' ? process.env.MIX_DEV_GOOGLE_API_KEY : process.env.MIX_PROD_GOOGLE_API_KEY,
 };
 
 export {
