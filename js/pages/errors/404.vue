3e031f98e1d9ee0d7bdea3728fe3a1b76969476c
dan1114ho <dan1114ho@gmail.com>
2018-08-09 10:42:58 -0400

    Fixed 404 page

--- js/pages/errors/404.vue
+++ js/pages/errors/404.vue
@@ -1,17 +1,32 @@
 <template>
-    <div class="text-center">
-        <h3 class="mb-4">Page Not Found</h3>
+    <div class="d-flex">
+        <div class="mx-auto p-5 mt-5 shadow-lg bg-white rounded" style="min-width: 60%">
+            <div class="text-center mb-5">
+                <img src="images/junket-logo.png" alt="Junket">
+            </div>
 
-        <div class="links">
-            <router-link :to="{ name: 'home' }">
-                Go Home
-            </router-link>
+            <div class="text-center">
+                <h3 class="mb-4">Oops An Error Has Occurred!</h3>
+                <h4 class="mb-5">404 Not Found</h4>
+
+                <div class="links">
+                    <router-link :to="{ name: 'home' }">
+                        <fa :icon="['fas', 'arrow-left']"></fa>
+                        Go Home
+                    </router-link>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'NotFound'
+    name: 'NotFound',
+
+    metaInfo () {
+        return { title: "Login" }
+    },
+
 }
 </script>
--- js/plugins/fontawesome.js
+++ js/plugins/fontawesome.js
@@ -3,7 +3,7 @@ import fontawesome from '@fortawesome/fontawesome'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 
 import {
-    faUser, faLock, faSignOutAlt, faCog
+    faUser, faLock, faSignOutAlt, faCog, faArrowLeft
 } from '@fortawesome/fontawesome-free-solid/shakable.es'
 
 import {
@@ -11,7 +11,7 @@ import {
 } from '@fortawesome/fontawesome-free-brands/shakable.es'
 
 fontawesome.library.add(
-    faUser, faLock, faSignOutAlt, faCog, faGithub, faInstagram, faFacebookF,faFacebook, faTwitter, faYoutube
+    faUser, faLock, faSignOutAlt, faCog, faGithub, faInstagram, faFacebookF,faFacebook, faTwitter, faYoutube, faArrowLeft
 )
 
 Vue.component('fa', FontAwesomeIcon)
